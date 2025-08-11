00abb52b590d97895fe0e24a0e070ce869991d29
muhbase <67451498+takanesachio@users.noreply.github.com>
2025-08-12 01:00:17 +0700

    fixing and improve

--- app/Http/Controllers/Superadmin/SChannelController.php
+++ app/Http/Controllers/Superadmin/SChannelController.php
@@ -10,6 +10,7 @@ use App\Models\GamblingDeposit;
 use App\Models\GamblingDepositAccount;
 use App\Models\Provider;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Validation\Rule;
 
 class SChannelController extends Controller
